11a50fe964b6003d757536d46f9aa78180f704f2
Javier Garcia Menendez <javiergme@servicioexterno.inditex.com>
2020-05-20 12:37:44 +0200

    Effects terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,14 @@ import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { appReducers } from './store/app.reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { EffectsArray } from './store/effects';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { AppComponent } from './app.component';
     SharedModule,
     UsuariosModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(EffectsArray),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
